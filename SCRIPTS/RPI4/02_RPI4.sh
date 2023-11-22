707eda1f367379c460a897984f4d14bb7a9e29d9
expoli <31023767+expoli@users.noreply.github.com>
2023-11-22 16:23:24 +0800

    Fix file paths and remove unnecessary code

--- SCRIPTS/RPI4/02_RPI4.sh
+++ SCRIPTS/RPI4/02_RPI4.sh
@@ -10,10 +10,10 @@ sed -i 's,-mcpu=generic,-march=armv8-a+crc,g' include/target.mk
 # Switch to rtl8169 driver
 #sed -i 's,kmod-r8169,kmod-r8168,g' target/linux/bcm27xx/image/bcm2711.mk
 # Addition-Trans-zh-master
-cp -rf ../PATCH/duplicate/addition-trans-zh-bcm27xx ./package/utils/addition-trans-zh
+# cp -rf ../PATCH/duplicate/addition-trans-zh-bcm27xx ./package/utils/addition-trans-zh
 # Add cputemp.sh and fix Apple iOS apns
-cp -rf ../PATCH/script/cputemp.sh ./package/base-files/files/bin/cputemp
-cp -rf ../PATCH/duplicate/files ./files
+# cp -rf ../PATCH/script/cputemp.sh ./package/base-files/files/bin/cputemp
+# cp -rf ../PATCH/duplicate/files ./files
 
 # Match Vermagic
 latest_release="$(curl -s https://api.github.com/repos/openwrt/openwrt/tags | grep -Eo "v23.05.+[0-9\.]" | head -n 1 | sed 's/v//g')"
